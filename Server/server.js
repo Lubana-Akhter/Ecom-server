28531ef9d584575961cbb5c0f081a36f4d4dbf34
Lubana-Akhter <lubana3093@gmail.com>
2023-06-16 10:10:20 +0600

    updated api

--- Server/server.js
+++ Server/server.js
@@ -20,16 +20,6 @@ app.use(helmet())
 // routes middleware
 readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`))) 
 
-// Mongo DB Database Connection
-// let URI="mongodb+srv://<username>:<password>@cluster0.7uslu.mongodb.net/todo?retryWrites=true&w=majority";
-// let URI="mongodb+srv://lubna93:gy3QxOf6v6gbDIan@cluster0.7uslu.mongodb.net/ecomDB?retryWrites=true&w=majority";
-// let OPTION={user:'lubna93',pass:'gy3QxOf6v6gbDIan',autoIndex:true}
-// mongoose.connect(URI,OPTION,(error)=>{
-//     console.log("Connection Success")
-//     console.log(error)
-// })
-
-
 // server
 const port = process.env.PORT || 8000;
 
